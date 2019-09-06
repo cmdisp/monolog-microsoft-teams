3996924d927a1c7e2cc5a68499f830ae7eedcf95
Kevin van den Bergh <kvdb@cm.nl>
2019-09-06 09:02:31 +0200

    Reformat

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-$autoloader_file = __DIR__ . '/../vendor/autoload.php';
-if (!is_readable( $autoloader_file )) {
-    die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer install' first." . PHP_EOL . PHP_EOL );
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+
+if (!is_readable($autoloader)) {
+    die(PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer install' first." . PHP_EOL . PHP_EOL);
 }
-require_once $autoloader_file;
+
+require_once $autoloader;
--- tests/src/TeamsLogHandlerTest.php
+++ tests/src/TeamsLogHandlerTest.php
@@ -1,53 +1,55 @@
-<?php 
-use Monolog\Logger;
-use Monolog\Handler\AbstractProcessingHandler;
+<?php
+
 use CMDISP\MonologMicrosoftTeams\TeamsLogHandler;
-use CMDISP\MonologMicrosoftTeams\TeamsMessage;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
 
-class TeamsLogHandlerTest extends \PHPUnit\Framework\TestCase
-{	
+class TeamsLogHandlerTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $incomingWebHookUrl;
 
-	public $incoming_webook_url;
-	public $loglevel = Logger::DEBUG;
+    /**
+     * @var int
+     */
+    private $loglevel = Logger::DEBUG;
 
+    public function setUp()
+    {
+        parent::setUp();
 
-	public function setUp()
-	{
-		parent::setUp();
-		if (!$this->incoming_webook_url = getenv('TEAMS_INCOMING_WEBHOOK_URL')) {
-			throw new \RuntimeException( "Please fill in TEAMS_INCOMING_WEBHOOK_URL in phpunit.xml");
-		}
-	}
+        if (!$this->incomingWebHookUrl = getenv('TEAMS_INCOMING_WEBHOOK_URL')) {
+            throw new RuntimeException('Please fill in TEAMS_INCOMING_WEBHOOK_URL in phpunit.xml');
+        }
+    }
 
+    private function createLogHandler()
+    {
+        return new TeamsLogHandler($this->incomingWebHookUrl, $this->loglevel);
+    }
 
-	public function createSut()
-	{
-		return new TeamsLogHandler( $this->incoming_webook_url, $this->loglevel );
-	}
+    public function testInstantiation()
+    {
+        $logHandler = $this->createLogHandler();
 
+        $this->assertInstanceOf(AbstractProcessingHandler::class, $logHandler);
+    }
 
-	public function testInstantiation()
-	{
-		$sut = $this->createSut();
-		$this->assertInstanceOf( AbstractProcessingHandler::class, $sut);
-	}
+    public function testUsage()
+    {
+        $logHandler = $this->createLogHandler();
 
+        $monolog = new Logger('TeamsLogHandlerTest');
+        $monolog->pushHandler($logHandler);
 
-	public function testUsage()
-	{
-		$sut = $this->createSut();
+        // "isHandling" will return FALSE when no handlers at all are registered with monolog.
+        $this->assertTrue($monolog->isHandling($this->loglevel));
 
-		$monolog = new Logger("TeamsLogHandlerTest");
-		$monolog->pushHandler( $sut );
-
-		// "isHandling" will return FALSE when no handlers at all 
-		// are registered with monolog.
-		$this->assertTrue( $monolog->isHandling( $this->loglevel ));
-
-		// Send a message
-		$result = $monolog->addRecord( $this->loglevel, 'test');
-		$this->assertTrue( $result );
-
-	}
-
+        // Send a message
+        $result = $monolog->addRecord($this->loglevel, 'test');
+        $this->assertTrue($result);
+    }
 }
